bc457b82717a73237c144524ea2b404c768f4c5a
S-Comic <samjule@outlook.com>
2018-02-22 22:55:30 +0000

    Navigation
    Added basic navigation between pages using the header bar.

--- MAIN.py
+++ MAIN.py
@@ -2,10 +2,13 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 @app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+def hello():
+    return render_template('hello.html')
 
 @app.route('/navigation/')
 def navi():
     return render_template('navigation.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
